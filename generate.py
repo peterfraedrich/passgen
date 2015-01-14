d6a87ff5b9c635c1ced538e47da857d6b9a506bc
Peter Fraedrich <pfraedrich@networkmgmt.com>
2015-01-14 13:59:49 -0500

    add logging function

--- generate.py
+++ generate.py
@@ -4,16 +4,28 @@ import os
 import random as rand
 import zerorpc
 import logging
+from datetime import datetime
+
+def log(message):
+	if os.path.exists('python.log'):
+		r = 'a'
+	else:
+		r = 'w+'
+	m = datetime.now().strftime('%Y:%M:%d %h:%m:%s -- ') + message
+	f = open('python.log',r)
 
 class RPC(object):
 	def generate(self,input):
 		def get_list():
-			w = open('wordlist','r')
-			x = w.readlines()
-			y = []
-			for i in x:
-				y.append(i.strip('\n'))
-			return y
+			try:
+				w = open('wordlist','r')
+				x = w.readlines()
+				y = []
+				for i in x:
+					y.append(i.strip('\n'))
+				return y
+			except:
+				log('unable to get wordlist')
 		
 		def random_word(list):
 			l = len(list)
@@ -30,8 +42,18 @@ class RPC(object):
 			passwords = result[0] + ' ' + result[1] + ' ' + result[2] + ' ' + result[3]
 			return passwords
 			print passwords
-		return main()	
+		return main()
 
-s = zerorpc.Server(RPC())
-s.bind('tcp://127.0.0.1:8081')
-s.run()
+if __name__ == '__main__':
+
+	s = zerorpc.Server(RPC())
+	try:
+		s.bind('tcp://127.0.0.1:8081')
+	except:
+		log('unable to bind port to local IP')
+		sys.exit(1)
+	try:
+		s.run()
+	except:
+		log('app failed to run')
+		sys.exit(1)
