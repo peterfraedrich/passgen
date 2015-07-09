ab4c463bf758e397f13258833523030cceee4b71
root <root@passgen.cvent.net>
2015-07-09 10:10:11 -0400

    added Apache/CGI version (WIP) and made separate folders

---
+++ apache/cgi-bin/generate.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import cgitb
+cgitb.enable()
+import os
+import logging
+import random as rand
+import sys
+import cgi
+from datetime import datetime
+
+def log(message):
+	if os.path.exists('/var/log/passgen.log'):
+		r = 'a'
+	else:
+		r = 'w+'
+	m = datetime.now().strftime('%Y:%M:%d %h:%m:%s -- ') + str(message) + '\n'
+	f = open('/var/log/passgen.log',r)
+	f.write(m)
+	f.close()
+
+def get_list():
+	try:
+		w = open('wordlist', 'r')
+		x = w.readlines()
+		y = []
+		for i in x:
+			y.append(i.strip('\n'))
+		return y
+	except:
+		log('unable to open or get wordlist [exception in passgen:get_list()]')
+
+def random_word(wordlist):
+	try:
+		l = len(wordlist) - 1
+		i = rand.randint(1,int(l))
+		return wordlist[i]
+	except:
+		log('unable to pick random word from wordlist [exception in passgen:random_word(wordlist)]')
+
+def main():
+	try:
+		result = []
+		wordlist = get_list()
+		for x in range(0,4):
+			r = random_word(wordlist)
+			result.append(r)
+		passwords = result[0] + ' ' + result[1] + ' ' + result[2] + ' ' + result[3]
+		return passwords
+	except:
+		log('unable to generate random password list [exception in passgen:main()]')
+
+if __name__ == '__main__':
+	try:
+		password = main()
+		print 'Content-Type: text/html'
+		print ''
+		print '<h2>' + str(password) + '</h2>'
+	except:
+		log('exception generated in __main__, unable to complete script [exception in passgen:__main__]')
---
+++ node/generate.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import os
+import random as rand
+import zerorpc
+import logging
+from datetime import datetime
+import sys
+
+def log(message):
+	if os.path.exists('python.log'):
+		r = 'a'
+	else:
+		r = 'w+'
+	m = datetime.now().strftime('%Y:%M:%d %h:%m:%s -- ') + message
+	f = open('python.log',r)
+
+class RPC(object):
+	def generate(self,input):
+		def get_list():
+			try:
+				w = open('wordlist','r')
+				x = w.readlines()
+				y = []
+				for i in x:
+					y.append(i.strip('\n'))
+				return y
+			except:
+				log('unable to get wordlist')
+		
+		def random_word(list):
+			l = len(list)
+			r = rand.randint(1,l)
+			return list[r]
+
+		def main():
+			loop = 0
+			result = []
+			list = get_list()
+			while loop < 4:
+				result.append(random_word(list))
+				loop = loop + 1
+			passwords = result[0] + ' ' + result[1] + ' ' + result[2] + ' ' + result[3]
+			return passwords
+			print passwords
+		return main()
+
+if __name__ == '__main__':
+
+	s = zerorpc.Server(RPC())
+	try:
+		s.bind('tcp://127.0.0.1:8081')
+	except:
+		log('unable to bind port to local IP')
+		sys.exit(1)
+	try:
+		s.run()
+	except:
+		log('app failed to run')
+		sys.exit(1)
